155c26e9ea38a34791cd97712d44143aa77fdafd
TomDawson <tomdaw92@gmail.com>
2023-02-06 19:40:59 +1000

    Added asset files and style changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,1 @@
-let mouseCursor = document.querySelector(".cursor");
-let links = document.querySelectorAll("a");
-
-document.addEventListener("mousemove", (e) => {
-  mouseCursor.style.top = e.pageY + "px";
-  mouseCursor.style.left = e.pageX + "px";
-});
-
-links.forEach((link) => {
-  link.addEventListener("mouseover", () => {
-    mouseCursor.classList.add("link-grow");
-  });
-  link.addEventListener("mouseleave", () => {
-    mouseCursor.classList.remove("link-grow");
-  });
-});
+console.log('- .... . .-. . -....- .. ... -....- -. --- -....- .--- --- -.- . -....- .... . .-. .');
