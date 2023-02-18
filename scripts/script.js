7609a49f902d4023349fabf77d47ef9da0cb52fe
TomDawson <tomdaw92@gmail.com>
2023-02-18 13:00:24 +1000

    Added crt image and chromatic abberation

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 console.log('- .... . .-. . -....- .. ... -....- -. --- -....- .--- --- -.- . -....- .... . .-. .');
 
-/* * * Theme Swapping * * */
+//// Theme Swapping
 
 var themeToggle = document.getElementById("theme-toggle");
 var darkModeSpan = document.getElementById("dark-mode-span");
 var lightModeSpan = document.getElementById("light-mode-span");
 var socialIcons = document.getElementsByClassName("social-icon");
 
-/* Retrieve theme preference from device */
+// Retrieve theme preference from device
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 if (storedTheme)
     document.documentElement.setAttribute('data-theme', storedTheme);
 
-/* Toggle between light and dark */
+// Toggle between light and dark
 themeToggle.onclick = function() {
     var currentTheme = document.documentElement.getAttribute("data-theme");
     var targetTheme = "light";
@@ -21,7 +21,7 @@ themeToggle.onclick = function() {
         targetTheme = "dark";
         lightModeSpan.style.display = "none";
         darkModeSpan.style.display = "block";
-        /* Loop through array of social icons to invert them */
+        // Loop through array of social icons to invert them
         for(var i=0; i < socialIcons.length; i++) {
             socialIcons[i].style.filter = "invert(100%)";
         }
@@ -29,7 +29,7 @@ themeToggle.onclick = function() {
         targetTheme = "light";
         darkModeSpan.style.display = "none";
         lightModeSpan.style.display = "block";
-        /* Loop through socials array to unset filter */
+        // Loop through socials array to unset filter
         for(var i=0; i < socialIcons.length; i++) {
             socialIcons[i].style.filter = "unset";
         }
@@ -39,9 +39,9 @@ themeToggle.onclick = function() {
     localStorage.setItem('theme', targetTheme);
 };
 
-/* Apply correct theme button on pageload */
+// Apply correct theme button on pageload
 function refreshThemeToggle() {
-    if (darkModeSpan && lightModeSpan) { /* Null check */
+    if (darkModeSpan && lightModeSpan) { // Null check
         if (storedTheme === "dark") {
             darkModeSpan.style.display = "block";
             lightModeSpan.style.display = "none";
@@ -57,7 +57,7 @@ function refreshThemeToggle() {
 
 refreshThemeToggle();
 
-/*** Scanlines ***/
+//// Scanlines 
 
 var $scanlines = $('.scanlines');
 $scanlines
