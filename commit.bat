41b35363d017a11f1b014db2f5170f4c7d746231
TomDawson <tomdaw92@gmail.com>
2023-02-21 11:39:25 +1000

    final final .bat fixes

--- commit.bat
+++ commit.bat
@@ -1,15 +1,9 @@
 set /p "comment=Enter comment > " & ::Prompt user for commit comment
 @echo off & ::Remove CMD output until @echo on
-
-::Delete previous combined files in root directory
-del .\combined.js
+del .\combined.js & ::Delete previous combined files in root directory
 del .\combined.css
-
-::Specify files in correct order for intentional sequencing
-type .\scripts\*.js >> .\combined.js & ::Concatenate JS files into combined.js
-
-type .\styles\*css >> .\combined.css & ::Concatenate CSS files into combined.css
-
+type .\scripts\*.js >> .\combined.js & ::Concatenate .js and .css files
+type .\styles\*css >> .\combined.css & ::Specify files in specific load order
 @echo on & ::Add, commit with previous comment, then push to repo with cmd feedback
 git add .
 git commit -m "%comment%"
