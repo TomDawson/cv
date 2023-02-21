b6ca4cf983e81f22980a01886c78153130b4d9f2
TomDawson <tomdaw92@gmail.com>
2023-02-21 11:06:05 +1000

    fix .bat del directory errors

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ##[tomdawson.dev](https://tomdawson.dev)
 This repo and the live url above represent the cv of this developer; Tom Dawson.
-The contents of this repo are intended to reflect 
--- commit.bat
+++ commit.bat
@@ -2,8 +2,8 @@
 set /p "comment=Your commit comment: "
 
 # Delete previous combined files
-del .\scripts\combined.js
-del .\styles\combined.css
+del .\combined.js
+del .\combined.css
 
 # Concatenate JS files into combined.js
 # Specify files in correct order for intentional sequencing
