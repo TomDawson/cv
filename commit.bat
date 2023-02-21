9fe1a4e8a9a23924f6175c134694313820131a7e
TomDawson <tomdaw92@gmail.com>
2023-02-21 11:41:04 +1000

    you thought I was really finished? more .bat changes

--- commit.bat
+++ commit.bat
@@ -1,10 +1,9 @@
+@echo off & ::Remove CMD output
 set /p "comment=Enter comment > " & ::Prompt user for commit comment
-@echo off & ::Remove CMD output until @echo on
 del .\combined.js & ::Delete previous combined files in root directory
 del .\combined.css
 type .\scripts\*.js >> .\combined.js & ::Concatenate .js and .css files
 type .\styles\*css >> .\combined.css & ::Specify files in specific load order
-@echo on & ::Add, commit with previous comment, then push to repo with cmd feedback
-git add .
+git add . & ::Add, commit with previous comment, then push to repo with cmd feedback
 git commit -m "%comment%"
 git push
